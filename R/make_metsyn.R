2d681d0ca9f9a34c8a7992eab02a9d5f2298a9cd
paulponcet <paulponcet@yahoo.fr>
2018-11-15 23:57:18 +0000

    Update doc and badges

--- R/make_metsyn.R
+++ R/make_metsyn.R
@@ -18,11 +18,9 @@
 #' Meteo France, see \href{https://donneespubliques.meteofrance.fr/?fond=produit&id_produit=90&id_rubrique=32}{here}. 
 #' 
 #' @note 
-#' This dataset is distributed by Meteo France under the terms of the 
-#' \href{https://www.etalab.gouv.fr/wp-content/uploads/2014/05/Open_Licence.pdf}{Open Licence 1.0}, 
-#' provided by \href{https://www.etalab.gouv.fr/en/qui-sommes-nous}{Etalab} and 
-#' designed to be compatible with the "Creative Commons Attribution 2.0" 
-#' (CC-BY 2.0) licence of Creative Commons. 
+#' These data are distributed by Meteo France under the terms of the Open 
+#' Licence, provided by Etalab and designed to be compatible with the 
+#' "Creative Commons Attribution 2.0" (CC-BY 2.0) licence of Creative Commons. 
 #' Etalab is the task force under the French Prime Minister's authority 
 #' leading Open Government Data policy for France. 
 #' 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # metsyn: Interface with the Meteo France Synop data API
 
-[![Travis-CI Build Status](https://travis-ci.org/paulponcet/metsyn.svg?branch=master)](https://travis-ci.org/paulponcet/metsyn) [![CRAN\_Status\_Badge](https://www.r-pkg.org/badges/version/metsyn)](https://cran.r-project.org/package=metsyn)
+[![Travis-CI Build Status](https://travis-ci.org/paulponcet/metsyn.svg?branch=master)](https://travis-ci.org/paulponcet/metsyn) [![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/metsyn)](https://cran.r-project.org/package=metsyn) [![](https://cranlogs.r-pkg.org/badges/metsyn)](https://cran.r-project.org/package=metsyn)
 
 
 ## Installation
